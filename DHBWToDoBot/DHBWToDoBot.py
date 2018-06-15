b76cff4d87ea6739cb39b9de90be7f7836633325
Menrma <menrathmarkus@gmail.com>
2018-06-15 16:38:39 +0200

    Add recognition of two intents

--- DHBWToDoBot/DHBWToDoBot.py
+++ DHBWToDoBot/DHBWToDoBot.py
@@ -1,12 +1,16 @@
 import os
-import json
+import sys
 
 from flask import Flask
 from flask import request
+from flask import make_response
+from flask import jsonify
 
 from DatabaseHelper import DatabaseHelper
 
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), "Database\TodoDB.db")
+
+TEST_TELEGRAM_ID = "123456789"
 
 app = Flask(__name__)
 
@@ -17,41 +21,58 @@ def dialogflowWebhook():
 
 	req = request.get_json(silent=True, force=True)
 
-	print("Request:" )
-	print(json.dumps(req, indent=4))
-
-	processRequest(req)
-
-
+	ret = processRequest(req)
+	ret = __createSimpleTextResponse(ret)
+	r = make_response(jsonify(ret))
+	return r
 
 #Method for processing the Request
 def processRequest(req):
-	# Get name of current intent
-	intentName = req.get("queryResult").get("intent").get("displayName")
+	dbHelper = None
+	try:
+		dbHelper = DatabaseHelper(DATABASE_PATH)
+		response = None
+		# Get name of current intent
+		intentName = req.get("queryResult").get("intent").get("displayName")
 
-	#if intentName == "Begruessung Intent":
+		if intentName == "Begruessung Intent":
+			response = __processBegruessungIntent(dbHelper)
+		elif intentName == "Termin abfragen":
+			# Get date from request
+			date = req.get("queryResult").get("parameters").get("date")
+			date = date[:10]
+			response = __processTerminAbfragenIntent(dbHelper, date)
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+	finally:
+		dbHelper.closeConnection()
 
+	return response
+	
+def __createSimpleTextResponse(data):
+	return {"fulfillmentText" : data};
 
+def __convertDateForOutput(date):
+	dateSplit = date.split('-')
+	return "{0}.{1}.{2}".format(dateSplit[2], dateSplit[1], dateSplit[0])
+
+def __processBegruessungIntent(dbHelper):
+	dbResult = dbHelper.checkUser(TEST_TELEGRAM_ID)
+	if dbResult:
+		username = dbResult[0][1]
+		return "Hallo {0}, wie kann ich dir helfen?".format(username)
+	else:
+		return "Hallo, ich bin Dein persönlicher Assistent und werde Dich bei der Planung Deiner Termine unterstützen. Möchtest Du einen Termin erstellen?"
+
+def __processTerminAbfragenIntent(dbHelper, date):
+		dateOutput = __convertDateForOutput(date)
+		dbResult = dbHelper.selectDayTodo(TEST_TELEGRAM_ID, date)
+		if(dbResult):
+			return None
+		else:
+			return "Für den {0} sind keine Aufgaben vorhanden".format(dateOutput)
 
 # Entry point of the application
 if __name__ == "__main__":
 	print("Starting Application...")
 	app.run()
-	#dbHelper = DatabaseHelper(DATABASE_PATH)
-	##dbHelper.insertUser("Leyla", "123456789")
-
-	#telegramID = "123456789"
-
-	#res = dbHelper.checkUser(telegramID)
-	#if(res):
-	#	print("### TODAY ###")
-	#	todayTodos = dbHelper.selectDayTodo(telegramID)
-	#	print(todayTodos)
-	#	print("### WEEK ###")
-	#	weekTodos = dbHelper.selectWeekTodo(telegramID)
-	#	print(weekTodos)
-	#	print("### SPECIFIC DATE ###")
-	#	dayTodos = dbHelper.selectDayTodo(telegramID, '2015-05-31')
-	#	print(dayTodos)
-
-	#	dbHelper.closeConnection()
